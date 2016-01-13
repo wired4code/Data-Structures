9d1435e34523c2076168548e5c76e4964d7be75f
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-13 19:24:40 -0200

    Complete functional queue.js to pass all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,28 @@ var Queue = function(){
   var storage = {};
 
   // Implement the methods below
+  var counter=0;
 
   someInstance.enqueue = function(value){
+    counter++;
+    storage[counter]=value;
   };
 
   someInstance.dequeue = function(){
+    var result=storage[1];
+    delete storage[1]
+
+    for(var i=0; i<=counter; i++){
+      storage[i]=storage[i+1];
+    }
+    if(counter){
+      counter--;
+    }
+    return result;
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
