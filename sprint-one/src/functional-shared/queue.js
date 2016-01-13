71e04595233951451417b0f168eaea2bfeb7c052
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-13 21:54:40 -0200

    Complete functional-shared queue.js to pass all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,43 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //
+  var obj = {};
+  extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
 
+  counter: 0,
 
+  enqueue: function(value){
+    this.counter++;
+    this[this.counter] = value;
+  },
+
+  dequeue: function(){
+    if(this.counter){
+      this.counter--;
+    }
+    var temp = this[1];
+    delete this[1];
+
+    for(var i = 1; i <= this.counter; i++){
+      this[i] = this[i+1];
+    }
+
+    return temp;
+  },
+
+  size: function(){
+    return this.counter;
+  }
+
+};
+
+var extend = function(to, from){
+  for(var key in from){
+    to[key] = from[key];
+  }
+}
