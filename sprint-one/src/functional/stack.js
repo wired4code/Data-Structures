3118dfc59c9daddc83a9d1e61f5c4a8bc61d578c
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-13 19:05:00 -0200

    Complete functional stack.js to pass all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,26 @@ var Stack = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  var counter=0;
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function(){
+    if(counter){
+      counter --;
+    }
+    var result = storage[counter+1];
+    delete storage[counter+1];
+    return result;
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
