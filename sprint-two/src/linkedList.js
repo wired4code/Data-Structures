6342c05149437fa021e10f05e2fa649e2043fd86
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-14 20:23:34 -0200

    Complete linkedList to pass all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+
+    var obj = new Node(value);
+    if(list.head === null && list.tail === null){
+      list.head = obj;
+      list.tail = obj;
+    } else {
+      var temp = list.tail;
+      list.tail = obj;
+      if(list.head.next === null){
+        list.head.next = obj;
+      }
+      temp.next = list.tail;
+    }
   };
 
   list.removeHead = function(){
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
+
   };
 
   list.contains = function(target){
+    if(list.head !== null && list.tail !== null){
+      var current = list.head;
+      while(current.next){
+        if(current.value === target){
+          return true;
+        }
+        current = current.next;
+      }
+      if(current.value === target){
+          return true;
+      }
+    }
+      return false;
   };
 
   return list;
