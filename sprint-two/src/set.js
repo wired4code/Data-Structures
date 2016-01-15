3df27c415dcfcebf43d10e92b8d0bb78d7258951
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-15 00:35:48 -0200

    Complete set.js to pass all tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  for(var i = 0; i < this._storage.length; i++){
+    if(this._storage[i] === item){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  for(var i = 0; i < this._storage.length; i++){
+    if(this._storage[i] === item){
+      this._storage.splice(this._storage[i], 1);
+    }
+  }
 };
 
 /*
