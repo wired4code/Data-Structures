396324089ae76f3fb3859f43afda4c10146439d4
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-15 18:20:06 -0200

    Complete binarySearchTree.js to pass all tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,73 @@
 var BinarySearchTree = function(value){
+  var obj = Object.create(Bstmethods);
+  obj.value = value;
+  obj.left=null;
+  obj.right=null;
+  return obj
+
 };
 
+var Bstmethods = {};
+
+Bstmethods.insert = function(value){
+  var current = this;
+  var node = new createNode(value, null, null);
+
+  while(current){
+    if(value < current.value){
+      if(current.left){
+        current = current.left;
+      } else {
+        current.left = node;
+        break;
+      }
+    } else {
+      if(current.right){
+        current = current.right;
+      } else {
+        current.right = node;
+        break;
+      }
+    }
+  }
+
+}
+
+
+Bstmethods.contains = function(value){
+  var current = this;
+  while(current){
+    if(current.value === value){
+      return true;
+    } else{
+      if(current.value > value){
+        current = current.left;
+      }else{
+        current = current.right;
+      }
+    }
+  }
+  return false;
+};
+
+Bstmethods.depthFirstLog = function(cb, node){
+
+  node = node || this;
+  cb(node.value);
+  if(node.left){
+    this.depthFirstLog(cb, node.left);
+  }
+  if(node.right){
+    this.depthFirstLog(cb, node.right);
+  }
+
+};
+
+var createNode = function(value, left, right){
+  this.value = value;
+  this.left = left;
+  this.right = right;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
