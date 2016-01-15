3f01bc434ae7c139d19c92cd6fa61808d67731ea
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-14 22:29:39 -0200

    Complete tree.js to pass all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,42 @@
 var Tree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+	//child is an object
+  var newT = Tree(value);
+  this.children.push(newT);
 
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target, node){
+  node = node || this;
+  var temp = false;
 
+
+    for(var i = 0; i < node.children.length; i++){
+
+      if(node.children[i].value === target){
+        return true;
+      }
+      if(node.children[i].children.length){
+// need to return recursive fxn to temp so it can return a value
+       temp = node.contains(target, node.children[i]);
+       if(temp === true){
+       	return true;
+       }
+      }
+    }
+
+  return temp;
 };
 
 
