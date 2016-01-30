95016f6ece7c8d19f4aad768950ee69ab4ea2e30
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-30 17:12:18 -0200

    Complete tests for hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,16 +4,49 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index);
+
+  if(!bucket){
+    bucket = [];
+    this._storage.set(index, bucket);
+    bucket.push([k,v]);
+  }
+
+  else {
+    for(var i = 0; i < bucket.length; i++){
+      if(bucket[i][0] === k){
+        bucket[i][1] = v;
+      } else{
+        bucket.push([k, v]);
+      }
+    }
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  for(var i = 0; i < bucket.length; i++){
+    if(bucket[i][0] === k){
+      return bucket[i][1];
+    }
+  }
 
 };
 
 HashTable.prototype.remove = function(k){
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
 
+  for(var i = 0; i < bucket.length; i++){
+    if(bucket[i][0] === k){
+      bucket[i][1] = null;
+    }
+  }
 };
 
 
