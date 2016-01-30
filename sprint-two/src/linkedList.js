46bf59b29763e0697328d7d0e8223e5c8da27386
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-30 00:16:16 -0200

    Complete tests for linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = new Node(value);
+
+    if(list.head === null && list.tail === null){
+      list.tail = node;
+      list.head = node;
+    }
+    var currentTail = list.tail;
+    list.tail = node;
+    currentTail.next = list.tail;
+
   };
 
   list.removeHead = function(){
+
+    var currentHead = list.head;
+    var newHead = list.head.next;
+    delete list.head;
+    list.head = newHead;
+    return currentHead.value;
+
   };
 
   list.contains = function(target){
+    var result = false;
+
+    var search = function(node){
+      if(node.value === target){
+        result = true;
+      }
+      if(node.next){
+        search(node.next);
+      }
+    }
+    search(list.head);
+    return result;
   };
 
   return list;
