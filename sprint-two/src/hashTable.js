927d24cb427169a74c0a116f94d42f944446619c
Sonny Sheth <sonny.sheth@gmail.com>
2016-01-30 17:17:18 -0200

    Add time complexity for hashTable methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -53,4 +53,7 @@ HashTable.prototype.remove = function(k){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert: constant;
+ * retrieve: linear;
+ * remove: linear;
  */
